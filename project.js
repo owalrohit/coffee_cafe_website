59f6762e3b6f02269fa8d3f8e9ac6ffbc1e1683d
owalrohit <86915070+owalrohit@users.noreply.github.com>
2022-07-29 16:12:12 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,16 @@
+var currentIndex = 1;
+displaySlides(currentIndex);
+
+function setSlides(num) {
+    displaySlides(currentIndex += num);
+}
+function displaySlides(num) {
+    var x;
+    var slides = document.getElementsByClassName("mySlides");
+    if (num > slides.length) { currentIndex = 1 }
+    if (num < 1) { currentIndex = slides.length }
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+    }
+    slides[currentIndex - 1].style.display = "block";
+}
